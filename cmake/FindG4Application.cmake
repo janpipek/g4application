e3b86dbf0c48adbfdfc78cbb15fffb951e3839e5
Jan Pipek <jan.pipek@gmail.com>
2013-11-09 23:04:18 +0100

    Use env.variable G4APPLICATION_DIR in cmake

--- cmake/FindG4Application.cmake
+++ cmake/FindG4Application.cmake
@@ -4,10 +4,10 @@
 #
 # It requires you to set the G4APPLICATION_DIR to point to the 
 #   g4Application root directory.
-if (NOT G4APPLICATION_DIR )
-    message(send_error "Please set G4APPLICATION_DIR variable.")
+if (NOT DEFINED ENV{G4APPLICATION_DIR} )
+    message(ERROR "Please set G4APPLICATION_DIR variable.")
 else()
     message(STATUS "Using g4Application from ${G4APPLICATION_DIR}.")
-    find_path(G4Application_INCLUDE_DIR "Plugin.hh" HINTS "${G4APPLICATION_DIR}/include")
-    find_library(G4Application_LIBRARY g4Application HINTS ${G4APPLICATION_DIR})
+    find_path(G4Application_INCLUDE_DIR "Plugin.hh" HINTS "$ENV{G4APPLICATION_DIR}/include")
+    find_library(G4Application_LIBRARY g4Application HINTS $ENV{G4APPLICATION_DIR})
 endif()
