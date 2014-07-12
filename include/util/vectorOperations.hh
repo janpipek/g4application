25983d94c622957a6c37164981afe186b2fec624
Jan Pipek <jan.pipek@gmail.com>
2014-07-12 20:29:05 +0200

    Fix: vector operations (all 4 operators)

--- include/util/vectorOperations.hh
+++ include/util/vectorOperations.hh
@@ -1,6 +1,12 @@
 #ifndef VECTOROPERATIONS_HH
 #define VECTOROPERATIONS_HH
 
+/**
+  * @short A few utilities for better work with numerical vectors.
+  *
+  * Especially operators.
+  */
+
 #include <vector>
 
 namespace g4
@@ -23,20 +29,47 @@ namespace g4
         }
 
         /**
-        * @short Divide vector by a constant.
-        *
-        * Useful when converting a vector to a different unit system.
-        */
+          * @short Divide vector by a constant.
+          *
+          * Useful when converting a vector to a different unit system.
+          */
         template <typename T> std::vector<T> operator/ (const std::vector<T>& aVector, const T& value)
         {
             std::vector<T> result;
             for (auto it = aVector.begin(); it != aVector.end(); it++)
             {
-                result.push_back(value * (*it));
+                result.push_back((*it) / value );
             }
             return result;
         }
-    }
+
+        /**
+          * @short Add a constant to a vector.
+          */
+        template <typename T> std::vector<T> operator+ (const std::vector<T>& aVector, const T& value)
+        {
+            std::vector<T> result;
+            for (auto it = aVector.begin(); it != aVector.end(); it++)
+            {
+                result.push_back((*it) + value);
+            }
+            return result;
+        }
+
+
+        /**
+          * @short Subtract a constant from a vector.
+          */
+        template <typename T> std::vector<T> operator- (const std::vector<T>& aVector, const T& value)
+        {
+            std::vector<T> result;
+            for (auto it = aVector.begin(); it != aVector.end(); it++)
+            {
+                result.push_back((*it) - value);
+            }
+            return result;
+        }
+    } 
 }
 
 #endif // VECTOROPERATIONS_HH
