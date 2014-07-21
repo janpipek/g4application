1434f7c80cf6e86f9a79730e0f218fdcb0a394b8
Jan Pipek <jan.pipek@gmail.com>
2014-07-21 20:01:03 +0200

    Vector operations: adding vectors, casting vectors

--- include/util/vectorOperations.hh
+++ include/util/vectorOperations.hh
@@ -8,6 +8,7 @@
   */
 
 #include <vector>
+#include <stdexcept>
 
 namespace g4
 {
@@ -107,6 +108,40 @@ namespace g4
             for (auto it = aVector.begin(); it != aVector.end(); it++)
             {
                 result.push_back(value - (*it));
+            }
+            return result;
+        }
+
+        /**
+          * @short Add two vectors.
+          *
+          * The vectors have to be of the same type and size.
+          */
+        template <typename T> std::vector<T> operator+ (const std::vector<T>& first, const std::vector<T>& second)
+        {
+            if (first.size() != second.size())
+            {
+                throw std::runtime_error("The vector have to be of same size.");
+            }
+            std::vector<T> result;
+            auto it1 = first.begin();
+            auto it2 = second.begin();
+            for (; it1 != first.end(); it1++, it2++)
+            {
+                result.push_back((*it1) + (*it2));
+            }
+            return result;
+        }
+
+       /**
+         * @short Retype a vector as vector of another type.
+         */
+        template <typename T, typename U> std::vector<U> convert_to(const std::vector<T>& aVector)
+        {
+            std::vector<U> result;
+            for (auto it = aVector.begin(); it != aVector.end(); it++)
+            {
+                result.push_back(static_cast<U>(*it));
             }
             return result;
         }
