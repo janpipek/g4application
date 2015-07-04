acd9c9704127f8b4674e3299a87da123678ad51d
Jan Pipek <jan.pipek@gmail.com>
2015-07-04 23:14:13 +0200

    CompositeDetectorConstruction class instead of CompositeGeometry

---
+++ include/CompositeDetectorConstruction.hh
@@ -0,0 +1,27 @@
+#ifndef COMPOSITEDETECTORCONSTRUCTION_HH
+#define COMPOSITEDETECTORCONSTRUCTION_HH
+
+#include "G4VUserDetectorConstruction.hh"
+
+class G4VPhysicalVolume;
+
+namespace g4
+{
+    class ComponentManager;
+
+    class CompositeDetectorConstruction : public G4VUserDetectorConstruction
+    {
+    public:
+        CompositeDetectorConstruction(ComponentManager* manager, G4VPhysicalVolume *worldVolume = nullptr);
+
+        virtual G4VPhysicalVolume* Construct();
+
+    private:
+        ComponentManager* _componentManager;
+
+        G4VPhysicalVolume* _worldVolume;
+    };
+}
+
+#endif // COMPOSITEDETECTORCONSTRUCTION_HH
+
---
+++ src/CompositeDetectorConstruction.cc
@@ -0,0 +1,44 @@
+#include "CompositeDetectorConstruction.hh"
+
+#include "ComponentManager.hh"
+#include "Component.hh"
+#include "GeometryBuilder.hh"
+#include "G4VPhysicalVolume.hh"
+
+using namespace g4;
+
+CompositeDetectorConstruction::CompositeDetectorConstruction(ComponentManager* manager, G4VPhysicalVolume* worldVolume)
+    : _componentManager(manager), _worldVolume(worldVolume)
+{
+
+}
+
+G4VPhysicalVolume* CompositeDetectorConstruction::Construct()
+{
+    auto components = _componentManager->GetComponents();
+
+    // Construct world (just one of the components should have it)
+    for (auto it = components.begin(); it != components.end(); it++)
+    {
+        Component* component = *it;
+        G4VPhysicalVolume* componentWorld = component->CreateGeometryBuilder()->CreateWorld();
+        if (componentWorld)
+        {
+            if (_worldVolume)
+            {
+                // TODO: Error
+            }
+            else
+            {
+                _worldVolume = componentWorld;
+            }
+        }
+    }
+
+    // Construct the geometry of components
+    for (auto it = components.begin(); it != components.end(); it++)
+    {
+        Component* component = *it;
+        component->CreateGeometryBuilder()->BuildGeometry(_worldVolume->GetLogicalVolume());
+    }
+}
