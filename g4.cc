7063e64ea85f5b4becceb74025262c8c55007fb7
Jan Pipek <jan.pipek@gmail.com>
2015-11-17 13:45:42 +0100

    Use optionparser

--- g4.cc
+++ g4.cc
@@ -3,39 +3,53 @@
 #include "globals.hh"
 
 #include "Application.hh"
+#include "optionparser.h"
 
 using namespace g4;
 using namespace std;
 
+enum optionIndex { UNKNOWN, INTERACTIVE, HELP };
+
+const option::Descriptor usage[] =
+{
+    {UNKNOWN, 0, "", "", option::Arg::None, "Usage: g4 [options] <macro1> <macro2> ...\n\nOptions:"},
+    {HELP, 0, "h", "help", option::Arg::None,               "  --help           Print usage and exit."},
+    {INTERACTIVE, 0, "i", "interactive", option::Arg::None, "  --interactive    Start interactive mode."},
+    {0,0,0,0,0,0}
+};
+
 /**
   * @short Simple main function.
-  *
-  * It only initializes and runs application and
-  * prints out all exceptions.
   */
 int main(int argc, char** argv)
 {
-    /*try
-    {*/
-        // Run the application
-        Application& app = Application::CreateInstance(argc, argv);
-        app.RunUI();
-    /*}
-    catch (const exception& exc)
-    {
-        G4cerr << "Exception thrown: " << exc.what() << endl;
-        exit(-1);
+    argc -= (argc>0); argv += (argc>0);
+    option::Stats  stats(usage, argc, argv);
+    option::Option options[stats.options_max], buffer[stats.buffer_max];
+    option::Parser parse(usage, argc, argv, options, buffer);
+
+    if (parse.error()) return 1;
+
+    if (options[HELP] || argc == 0) {
+        option::printUsage(std::cout, usage);
+        return -1;
     }
-    catch (const string& exc)
+
+    Application& app = Application::CreateInstance(argc, argv);
+
+    // Load all macros from command line
+    for (int i = 0; i < parse.nonOptionsCount(); ++i)
     {
-        G4cerr << "Exception thrown: " << exc << endl;
-        exit(-1);
+        app.AddMacro(parse.nonOption(i));
     }
-    catch (const char* exc)
+
+    app.RunUI();
+
+    if (options[INTERACTIVE])
     {
-        G4cerr << "Exception thrown: " << exc << endl;
-        exit(-1);
-    }*/
+        app.EnterInteractiveMode();
+    }
+
     G4cout << "Application exiting normally..." << G4endl;
 }
 
